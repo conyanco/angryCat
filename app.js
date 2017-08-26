93da7a859e9fd9cbc04d7b9f29328bf71e978c2b
conyanco <n.namatame@gmail.com>
2017-08-26 14:00:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let http = require('http');
-let express = require('express');
+//let express = require('express');
 
 const URL = 'http://productsearch.linksynergy.com/productsearch%3Ftoken=b0861c6d36b799b6355b4d1729a141c0d9dcab69272900f8d6f0822a3690e7e9&keyword=%2522%E3%83%95%E3%82%A1%E3%83%83%E3%82%B7%E3%83%A7%E3%83%B3%2522';
 const app = express();
@@ -21,10 +21,11 @@ http.get(URL, (res) => {
 }).on('error', (e) => {
   console.log(e.message)
 });
-
+/*
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 	});
 app.listen(3000, function () {
 	console.log('listening on port 3000');
 	});
+*/
